14f1206c52c3bff854f07c654f91b0142297440b
TavlaLasha <lashatavlalashvili@yahoo.com>
2021-12-11 23:05:39 +0400

    Added Client APP

--- TTWebApp/TTWebApp/urls.py
+++ TTWebApp/TTWebApp/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('TTRestAPI.urls')),
+    path('', include('TTClientApp.urls')),
 ]
